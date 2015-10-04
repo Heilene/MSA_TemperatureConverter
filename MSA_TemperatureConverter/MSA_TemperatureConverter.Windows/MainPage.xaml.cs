da7738c5050570761a70dda29d87493b69c9578c
Heilene <heilenedaem@gmail.com>
2015-10-05 12:34:06 +1300

    Upload Project Master

---
+++ MSA_TemperatureConverter/MSA_TemperatureConverter.Windows/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MSA_TemperatureConverter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        TempCalculation Temp = new TempCalculation();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GetTempConversion(object sender, RoutedEventArgs e)
+        {
+            //Pass Textbox value to the input value for my calculation class
+            Temp.DblInput = Convert.ToDouble(TxtInput.Text);
+
+            if (CbFahToCel.IsChecked.HasValue && CbFahToCel.IsChecked.Value )
+            {
+                //Output conversion result for Celsius rounding up to 2 decimal points
+               TextBlockOutput.Text = String.Format("{0} Fahrenheit is equal to {1} Celsius", Temp.DblInput, Temp.DblCel.ToString("F1"));
+
+               //Display text colour of output according to temperature 
+               if (Temp.DblCel < 20)
+               {
+                   TextBlockOutput.Foreground = new SolidColorBrush(Colors.Blue);
+               }
+               else
+               {
+                   TextBlockOutput.Foreground = new SolidColorBrush(Colors.Red);
+               }
+                
+               
+            }
+            else if (CbCelToFah.IsChecked.HasValue && CbCelToFah.IsChecked.Value)
+            {
+                //Output conversion result for Farenheit rounding up to 2 decimal points
+                TextBlockOutput.Text = String.Format("{0} Celsius is equal to {1} Fahrenheit", Temp.DblInput, Temp.DblFah.ToString("F1"));
+
+                //Display text colour of output according to temperature 
+                if (Temp.DblFah < 68)
+                {
+                    TextBlockOutput.Foreground = new SolidColorBrush(Colors.Blue);
+                }
+                else
+                {
+                    TextBlockOutput.Foreground = new SolidColorBrush(Colors.Red);
+                }
+               
+            }
+            
+        }
+
+        private void UncheckCelCb(object sender, RoutedEventArgs e)
+        {
+            //sets Celsius checkbox value to unchecked
+            CbCelToFah.IsChecked = false;
+        }
+
+        private void UncheckedFahCb(object sender, RoutedEventArgs e)
+        {
+            //Sets Farhenheit checkbox value to unchecked
+            CbFahToCel.IsChecked = false;
+        }
+    }
+}
---
+++ MSA_TemperatureConverter/MSA_TemperatureConverter.Windows/TempCalculation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MSA_TemperatureConverter
+{
+    class TempCalculation
+    {
+        //==============Fields==================
+
+        private double _dblFah;
+        private double _dblCel;
+       
+
+        //==============Properties==============
+        public double DblInput { get; set; }
+        public ImageBrush MyBackGroundImage { get; set; }
+
+        public double DblCel
+        {
+            get
+            {
+              //calculate celsius conversion returns celsius value
+                _dblCel = (DblInput - 32) * 5 / 9;
+                return _dblCel;
+
+            } 
+        }
+
+        public double DblFah
+        {
+            get
+            {
+                //calculate fahrenheit conversion returns fahrenheit conversion
+                _dblFah = DblInput*9/5 + 32;
+                return _dblFah;
+            }
+        }
+
+
+
+    }
+}
